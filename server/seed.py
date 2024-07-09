f15ed1703d5a5d9b3d5851b2c8927b6716a60ae0
brianchege2k <chegebrian84@gmail.com>
2024-07-09 16:20:34 +0300

    lab complete

--- server/seed.py
+++ server/seed.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 from random import choice as rc
-
 from faker import Faker
 
 from app import app
@@ -14,7 +13,6 @@ if "Duane" not in usernames:
     usernames.append("Duane")
 
 def make_messages():
-
     Message.query.delete()
     
     messages = []
